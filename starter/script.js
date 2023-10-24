490ac90e44122f3e58a7b5e6e0b977669719161e
cnjeri <christine.njeri@aimsoft.co.ke>
2023-10-24 09:26:51 +0300

    Chore(Objects): Introduction to loops

--- starter/script.js
+++ starter/script.js
@@ -281,7 +281,7 @@ const totals = [bills[0]+tips[0],bills[1]+tips[1],bills[2]+tips[2] ]
 
 
 
-
+/********* 
 // OBJECTS
 // Here we define key value pairs where we give each value a name
 // Object literal syntax.
@@ -325,3 +325,78 @@ console.log(jonas);
 
 // const jonasFriends = jonas['friends'].length;
 console.log(`${jonas.firstName} has ${jonas.friends.length} and his best friend is called ${jonas.friends[0]}`);
+****/
+
+
+
+
+
+
+
+/********* 
+CHALLENGE #3
+Let's go back to Mark and John comparing their BMIs!
+
+This time, let's use objects to implement the calculations! Remember: BMI = mass / (height * height) (mass in kg and height in meters).
+
+Your tasks:
+
+For each of them, create an object with properties for their full name, mass, and height (Mark Miller and John Smith). Name these objects as mark and john, and their properties exactly as fullName, mass and height.
+
+Create a calcBMI method on each object to calculate the BMI (the same method on both objects). Assign the BMI value to a property, and also return it from the method.
+
+Log to the console who has the higher BMI, together with the full name and the respective BMI. Example: "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!".
+
+TEST DATA: Marks weighs 78 kg and is 1.69 m tall. John weighs 92 kg and is 1.95 m tall.
+
+
+
+👋 OPTIONAL: You can watch my solution in video format in the next lecture
+
+***********/
+
+/********* 
+const mark = {
+    fullName: 'Mark Miller',
+    mass:78,
+    height: 1.69,
+    calcBMI: function() {
+        this.bmi = this.mass/ this.height ** 2
+        return this.bmi
+    }
+
+};
+
+const john = {
+    fullName: 'John Smith',
+    mass:92,
+    height: 1.95,
+    calcBMI: function() {
+        this.bmi = this.mass/ this.height ** 2
+        return this.bmi
+    }
+
+};
+
+john.calcBMI();
+mark.calcBMI();
+
+console.log(mark.bmi, john.bmi);
+
+if(mark.bmi < john.bmi){
+    console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`); 
+  }else if(mark.bmi > john.bmi){
+      console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`);
+  }
+
+****/
+
+
+
+
+
+// LOOPS
+// for loop keeps running while condition is TRUE
+for(let rep = 1; rep <= 10; rep++){
+    console.log(`Lifting weights repetition ${rep} 🏋️`);
+}
