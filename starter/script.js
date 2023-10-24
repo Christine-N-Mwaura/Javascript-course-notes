79c2662a143d1f8cb0f4b6d08c567a03e43f7e26
cnjeri <christine.njeri@aimsoft.co.ke>
2023-10-24 18:37:40 +0300

    Chore(Loops): Learning while loops and for loops

--- starter/script.js
+++ starter/script.js
@@ -394,9 +394,177 @@ if(mark.bmi < john.bmi){
 
 
 
-
+/************* 
 // LOOPS
 // for loop keeps running while condition is TRUE
 for(let rep = 1; rep <= 10; rep++){
     console.log(`Lifting weights repetition ${rep} 🏋️`);
 }
+
+const jonas = [
+    'Jonas',
+    'Script',
+    2037 - 1991,
+    'teacher',
+    ['Michael','Peter', 'Steven']
+];
+
+const types = [];
+
+for(let i = 0; i < jonas.length; i++){
+    // Reading from jonas array
+    console.log(jonas[i],typeof jonas[i]);
+
+    // Filling types array
+     types[i] = typeof jonas[i];
+
+     types.push(typeof jonas[i]);
+}
+
+console.log(types);
+
+const years = [1991, 2007, 1969, 2020];
+const ages = []
+
+for(let i = 0; i < years.length; i++){
+    ages.push(2037 - years[i])
+}
+
+console.log(ages);
+
+// CONTINUE AND BREAK
+// Continue - Exit the current iteration of the loop and continue to the next one
+// Break - Completely terminate the whole loop.
+
+console.log('------ONLY STRINGS-----');
+for(let i = 0; i < jonas.length; i++){
+    if(typeof jonas[i] !== 'string') continue
+    console.log(jonas[i],typeof jonas[i]);
+}
+
+console.log('------BREAK WITH NUMBER-----');
+
+for(let i = 0; i < jonas.length; i++){
+
+    if(typeof jonas[i] !== 'number') break
+
+    console.log(jonas[i],typeof jonas[i]);
+}
+*********/
+
+/****** 
+// LOOP BACKWARDS AND LOOP IN A LOOP
+
+const jonas = [
+    'Jonas',
+    'Script',
+    2037 - 1991,
+    'teacher',
+    ['Michael','Peter', 'Steven']
+];
+
+for(let i = jonas.length - 1; i >= 0; i--){
+    console.log(i, jonas[i]);
+}
+
+for(let exercise = 1; exercise < 4; exercise++){
+    console.log(`--------------Starting exercise ${exercise}`);
+
+    for(let rep = 1; rep < 6; rep++){
+        console.log(`Lifting weight repetition ${rep} 🏋️`);
+    }
+}
+
+*****/
+
+
+// WHILE LOOP
+// for(let rep = 1; rep <= 10; rep++){
+//     console.log(`Lifting weights repetition ${rep} 🏋️`);
+// }
+
+let rep = 1 ;
+while(rep <= 10){
+    // console.log(`WHILE:Lifting weights repetition ${rep} 🏋️`);
+    rep ++;
+}
+
+// while does not need a counter like a for loop. Whenever you don't know beforehand how many iterations you will need.
+let dice = Math.trunc(Math.random() * 6) + 1;
+console.log(dice);
+
+while(dice !== 6){ //
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+
+    if(dice === 6)console.log('Loop is about to end....');
+}
+
+/*********** 
+CHALLENGE #4
+Let's improve Steven's tip calculator even more, this time using loops!
+
+Your tasks:
+
+Create an array called bills containing all 10 test bill values.
+
+Create empty arrays for the tips and the totals (tips and totals)
+
+Use the calcTip function we wrote before (included in the starter code) to calculate tips and total values (bill + tip) for every bill value in the bills array. Use a for loop to perform the 10 calculations!
+
+
+
+TEST DATA: 22, 295, 176, 440, 37, 105, 10, 1100, 86, and 52.
+
+****/
+
+
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+  }
+  
+  /* Write your code below. Good luck! 🙂 */
+  
+  const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+  const tips = [];
+  const totals = [];
+  
+  for(let i = 0; i < bills.length; i++){
+      let tip = calcTip(bills[i]);
+      tips.push(tip)
+      totals.push(bills[i] + tip)
+  }
+  
+  console.log(tips, totals);
+
+/************* 
+BONUS:
+
+Write a function calcAverage which takes an array called arr as an argument. This function calculates the average of all numbers in the given array. This is a DIFFICULT challenge (we haven't done this before)! Here is how to solve it if you feel like it:
+
+First, you will need to add up all values in the array. To do the addition, start by creating a variable sum that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the sum variable. This way, by the end of the loop, you have all values added together.
+
+To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements).
+
+Call the function with the totals array.
+
+
+
+👋 OPTIONAL: You can watch my solution in video format in the next lecture
+*******/
+
+
+
+let sum = 0;
+const calcAverage = (arr) => {
+    for(let i = 0; i < arr.length; i++){
+
+        sum = sum + arr[i];
+
+    }
+    
+    return sum / arr.length
+}
+
+
+console.log(calcAverage(totals))
